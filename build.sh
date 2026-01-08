09cb9bd7f2a086e5b1d5e80516fc7fa43823bdb9
aisnota <sieun6213@gmail.com>
2026-01-08 13:34:51 +0000

    온프-기본 모니터링

--- build.sh
+++ build.sh
@@ -1,10 +1,10 @@
-docker build -t hyunhojang/auth_server:v2.01 ./auth_server
-docker build -t hyunhojang/employee_server:v2.01 ./employee_server
-docker build -t hyunhojang/gateway:v2.01 ./gateway
-docker build -t hyunhojang/photo_service:v2.01 ./photo_service
-docker build -t hyunhojang/frontend:v2.01 ./frontend
-docker push hyunhojang/auth_server:v2.01
-docker push hyunhojang/employee_server:v2.01
-docker push hyunhojang/gateway:v2.01
-docker push hyunhojang/photo_service:v2.01
-docker push hyunhojang/frontend:v2.01
+docker build -t hisieun/auth_server:v2.01 ./auth_server
+docker build -t hisieun/employee_server:v2.01 ./employee_server
+docker build -t hisieun/gateway:v2.01 ./gateway
+docker build -t hisieun/photo_service:v2.01 ./photo_service
+docker build -t hisieun/frontend:v2.01 ./frontend
+docker push hisieun/auth_server:v2.01
+docker push hisieun/employee_server:v2.01
+docker push hisieun/gateway:v2.01
+docker push hisieun/photo_service:v2.01
+docker push hisieun/frontend:v2.01
